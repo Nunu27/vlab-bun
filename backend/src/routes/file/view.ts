40f4e4c6a197a883ab1287bade8e57d40c44f2a5
Wisnu Agung Pambudi <wisnu.agung.001@gmail.com>
2025-11-27 23:34:47 +0700

    fix(file): remove server from header

--- backend/src/routes/file/view.ts
+++ backend/src/routes/file/view.ts
@@ -2,11 +2,13 @@ import { createRouter } from "@backend/plugins/services";
 
 export default createRouter().get(
 	"/:name",
-	async ({ session, params, storage, bucket, set, status }) => {
+	async ({ session, params, storage, status }) => {
 		if (!session) return status(401);
 
 		let response = await storage.getObjectResponse(params.name);
 		if (!response) return status(404);
+
+		response.headers.delete("server");
 
 		return response;
 	},
