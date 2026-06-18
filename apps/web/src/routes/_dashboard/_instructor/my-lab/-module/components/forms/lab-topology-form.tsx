4614870c8d0c6bfef23f8c6617b7a9e983197be1
Wisnu Agung Pambudi <wisnu.agung.001@gmail.com>
2026-06-18 21:13:39 +0700

    feat(web): add save topology template button

--- apps/web/src/routes/_dashboard/_instructor/my-lab/-module/components/forms/lab-topology-form.tsx
+++ apps/web/src/routes/_dashboard/_instructor/my-lab/-module/components/forms/lab-topology-form.tsx
@@ -5,12 +5,16 @@ import InterfaceSelectModal from "@web/shared/topology/components/modals/interfa
 import NodeProperties from "@web/shared/topology/components/properties";
 
 import { ApplyTopologyTemplateModal } from "../modals/apply-topology-template-modal";
+import { SaveTopologyTemplateModal } from "../modals/save-topology-template-modal";
 
 function LabTopologyForm() {
 	return (
 		<Card className="flex h-200 flex-col p-0">
 			<CardContent className="relative flex flex-1 p-0">
-				<ApplyTopologyTemplateModal />
+				<div className="absolute top-4 right-4 z-10 flex gap-2">
+					<SaveTopologyTemplateModal />
+					<ApplyTopologyTemplateModal />
+				</div>
 				<DevicePalette />
 				<TopologyCanvas />
 				<NodeProperties />
